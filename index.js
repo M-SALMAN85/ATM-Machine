0206b46917fc6a5543a75b2930ba578ba9808458
M-SALMAN85 <145777284+M-SALMAN85@users.noreply.github.com>
2024-04-02 02:35:22 +0500

    publish on npm

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let myBalance = 20000;
 let pinCode = 1254;
@@ -68,64 +69,3 @@ if (atm.pinNumber === pinCode) {
         console.log("Please Put Wright Pin Number");
     }
 }
-// import inquirer from "inquirer";
-// let myBalance = 20000;
-// let pinCode = 1254;
-// let atm = await inquirer.prompt([
-//   {
-//     message: "Enter Your Pin Number: ",
-//     type: "number",
-//     name: "pinNumber",
-//   }
-// ]);
-// if (atm.pinNumber === pinCode) {
-//     console.log("Right Code! ");
-//     let operationAnswer = await inquirer.prompt(
-//     [
-//         {
-//             message: "Please Select One Option",
-//             type: "list",
-//             name: "operation",
-//             choices: ["Withdraw", "Check Balance"]
-//         }
-//     ]
-//   );
-//     console.log(operationAnswer);
-// if (operationAnswer.operation === "Withdraw") {
-//     let enterAmount = await inquirer.prompt(
-//     [
-//         {
-//             message: "Select One Option for Cash Withdraw",
-//             type: "list",
-//             name: "select",
-//             choices: ["Please Put Amount", "Fast Cash"]
-//         }
-//     ]
-// );
-//     console.log(enterAmount);
-//         if (enterAmount.select === "Please Put Amount") {
-//             let fastCash = await inquirer.prompt(
-//             [
-//                 {
-//                     message: "Enter Your Amount",
-//                     type: "number",
-//                     name: "amount",                    
-//                 }
-//             ]
-//         );
-//                  console.log(fastCash);
-//                     if (fastCash.amount >= myBalance) {
-//                 console.log("insufficient Balance?");
-//                  } else if 
-//                      (myBalance -= fastCash.amount)
-//                  console.log("Your Current Balance is: " + myBalance);
-//         else if (enterAmount.select === "Fast Cash")
-//          console.log("Your Balance is:" + myBalance);
-//   }
-//     else if (operationAnswer.operation === "Check Balance")
-//     console.log("Your Balance is:" + myBalance);
-// } else {
-//   console.log("Please Put Wright Pin Number");
-// }
-// }
-// }
